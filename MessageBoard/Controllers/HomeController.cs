54548800c2ee533108fb8dc78af0b93dd9fc216f
Xiaochen GUO <xcguo@outlook.com>
2014-03-12 21:56:33 +0100

    Initial Project commit

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mailService;
+        public HomeController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From : {1}{0} Email: {2}{0} Website: {3}{0} Comment:{4}{0}", Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+            if (_mailService.SendMail("noReply@messageBoard.com",
+                "tianjian279@gmail.com", 
+                "Website Content", 
+                msg))
+           {
+               ViewBag.MailSent = true;
+           }
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+
+        [Authorize(Roles="Admin")]
+        public ActionResult Moderation()
+        {
+            return View();
+        }
+    }
+}
