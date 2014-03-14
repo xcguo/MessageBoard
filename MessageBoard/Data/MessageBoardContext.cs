a5af349fc823264fa3b7c2da9f5255107bcbc6c7
Xiaochen GUO <xcguo@outlook.com>
2014-03-14 19:05:23 +0100

    Add DB context and DB models

---
+++ MessageBoard/Data/MessageBoardContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MessageBoard.Data
+{
+    public class MessageBoardContext : DbContext
+    {
+        public MessageBoardContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
