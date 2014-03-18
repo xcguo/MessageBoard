6d101d5aee9e690ccd24026c2e5ebe06f7d46370
Xiaochen GUO <xcguo@outlook.com>
2014-03-18 01:54:56 +0100

    add singleTopic view

--- MessageBoard/js/home-index.js
+++ MessageBoard/js/home-index.js
@@ -1,7 +1,7 @@
 ﻿//home-index.js
 var module = angular.module("homeIndex", ["ngRoute"]);
 
-module.config(function ($routeProvider) {
+module.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when("/", {
         controller: "topicsController",
         templateUrl: "/templates/topicsView.html"
@@ -11,10 +11,14 @@ module.config(function ($routeProvider) {
         templateUrl: "/templates/newTopicView.html"
 
     });
+    $routeProvider.when("/message/:id", {
+        controller: "singleTopicController",
+        templateUrl:"/templates/singleTopicView.html"
+    });
     $routeProvider.otherwise({redirectTo:"/"});
-});
+}]);
 
-module.factory("dataService", function ($http, $q) {
+module.factory("dataService", ["$http", "$q", function ($http, $q) {
 
     var _topics = [];
     var _isInit = false;
@@ -42,7 +46,7 @@ module.factory("dataService", function ($http, $q) {
         return deferred.promise;
     };
 
-    var _addTopic = function(newTopic){
+    var _addTopic = function (newTopic) {
         var deferred = $q.defer();
         $http.post("/api/topics", newTopic)
        .then(function (result) {
@@ -57,18 +61,81 @@ module.factory("dataService", function ($http, $q) {
        });
 
         return deferred.promise;
+    };
+
+    function _findTopic(id) {
+        var found = null;
+
+        $.each(_topics, function (i, item) {
+            if (item.id == id) {
+                found = item;
+                return false; //break out the each
+            }
+        });
+        return found;
+    }
+
+    var _getTopicById = function (id) {
+        var deferred = $q.defer();
+
+        if (_isReady()) {
+            var topic = _findTopic(id);
+            if (topic) {
+                deferred.resolve(topic);
+            } else {
+                deferred.reject();
+            }
+        } else {
+            _getTopics()
+            .then(function () {
+                //success
+                var topic = _findTopic(id);
+                if (topic) {
+                    deferred.resolve(topic);
+                } else {
+                    deferred.reject();
+                }
+            },
+            function () {
+                //error
+                deferred.reject();
+            });
+        }
+
+        return deferred.promise;
+    }
+
+    var _saveReply = function (topic, newReply) {
+        var deferred = $q.defer();
+        $http.post("/api/topics/" + topic.id + "/replies", newReply)
+        .then(function (result) {
+            //success
+            if (topic.replies == null) {
+                topic.replies = [];
+            }
+            topic.replies.push(result.data);
+            deferred.resolve(result.data);
+        },
+        function () {
+            //error
+            deferred.reject();
+        });
+
+        return deferred.promise;
     }
 
     return {
         topics : _topics,
         getTopics: _getTopics,
         addTopic: _addTopic,
-        isReady :   _isReady
+        isReady: _isReady,
+        getTopicById: _getTopicById,
+        saveReply: _saveReply
     };
     
-});
+}]);
 
-function topicsController($scope, $http, dataService) {
+var topicsController = ["$scope", "$http", "dataService", function ($scope, $http, dataService) {
     $scope.data = dataService;
     $scope.isBusy = false;
     if (dataService.isReady() == false) {
@@ -85,9 +152,9 @@ function topicsController($scope, $http, dataService) {
                 $scope.isBusy = false;
             });
     }
-}
+}];
 
-function newTopicController($scope, $http, $window, dataService) {
+var newTopicController = ["$scope", "$http", "$window", "dataService", function($scope, $http, $window, dataService) {
     $scope.newTopic = {};
     $scope.save = function () {
         dataService.addTopic($scope.newTopic)
@@ -100,4 +167,33 @@ function newTopicController($scope, $http, $window, dataService) {
             alert("could not load topics");
         });
     }
-}
+}];
+
+var singleTopicController = ["$scope", "dataService", "$window", "$routeParams",function($scope, dataService, $window, $routeParams) {
+    $scope.topic = null;
+    $scope.newReply = {};
+
+    dataService.getTopicById($routeParams.id)
+    .then(function (topic) {
+        //success
+        $scope.topic = topic;
+    },
+    function () {
+        //error
+        $window.location = "#/";
+    });
+
+    $scope.addReply = function () {
+        dataService.saveReply($scope.topic, $scope.newReply)
+        .then(function () {
+            //success
+            $scope.newReply.body = "";
+        },
+        function () {
+            //error
+            alert("could not save the new reply");
+        });
+
+    };
+
+}];
